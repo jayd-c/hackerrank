f49a1807fab1aecdd33a75a75bc66975c76797dd
JayanDesilva <jaycdesilva@gmail.com>
2023-08-14 10:37:54 -0400

    between two sets completed

--- src/Easy/arrays/BetweenTwoSets.java
+++ src/Easy/arrays/BetweenTwoSets.java
@@ -13,64 +13,27 @@ public class BetweenTwoSets {
         List<Integer> b = new ArrayList<>(List.of(new Integer[] {16,32,96}));
 
         System.out.println(getTotalX(a,b));
+
     }
 
-    public static int getTotalX(List<Integer> a, List<Integer> b) {
-        int result = 0;
-        Collections.sort(a);
-        Collections.sort(b);
-
-        List<Integer> aOne = new ArrayList<>();
-        List<Integer> aTwo = new ArrayList<>();
-        int firstOfB = b.get(0);
-        for(int i = 1; i<=firstOfB; i++) {
-            if((a.get(0) * i) <= firstOfB){
-                aOne.add(a.get(0)*i);
-            }
+    private static int getGcd (int one, int two) {
+        if(two == 0) return one;
+        else {
+        return getGcd(two, one%two);
         }
-        for(int i = 1; i<=firstOfB; i++) {
-            if((a.get(1) * i) <= firstOfB){
-                aTwo.add(a.get(1)*i);
-            }
-        }
-        List<Integer> commonInts = new ArrayList<>();
-        System.out.println(aOne);
-        System.out.println(aTwo);
-        for(Integer i: aTwo) {
-//            System.out.println("i is " + i);
-            if(aOne.contains(i)) {
-                commonInts.add(i);
-            }
-        }
-        ArrayList<Integer> resultLIst = new ArrayList<>();
-        System.out.println("commonInts = " + commonInts);
-        for(Integer j: commonInts){
-            for(Integer k: b) {
-                if (k % j == 0) resultLIst.add(j);
-            }
-        }
-//        System.out.println("Result list " + resultLIst);
-        resultLIst.clear();
-        ArrayList<Integer> tempList = new ArrayList<>();
-        for(int i = 0; i<commonInts.size(); i++) {
-            int item = commonInts.get(i);
-            for(int j = 0; j<b.size(); j++) {
-                if(b.get(j) % item == 0) {
-                    System.out.println(item + " " + b.get(j));
-                    tempList.add(b.get(j));
-
-                }
-            }
-            if(tempList.size() == b.size()) {
-                result++;
-            }
-            tempList.clear();
-        }
-        return result;
     }
 
+    private static int getLcm (int one, int two) {
+        if(one == 0 || two == 0) {
+            return 0;
+        } else {
+            int gcd = getGcd(one,two);
+            return Math.abs(one * two / gcd);
 
-    public static int getTotalX2 (List<Integer> a, List<Integer> b) {
+        }
+    }
+
+    public static int getTotalX (List<Integer> a, List<Integer> b) {
 
         int result = 0;
         Collections.sort(a);
@@ -79,12 +42,12 @@ public class BetweenTwoSets {
         //get the lcm (lowest common multiplier) of list a
         int lcm = a.get(0);
         for(Integer i: a){
-            lcm = Math.min(lcm,i);
+           lcm = getLcm(lcm,i);
         }
         //get thd GCD of all elements of b
         int gcd = b.get(0);
         for(Integer i: b) {
-            gcd = Math.max(i,gcd);
+            gcd = getGcd(gcd,i);
         }
         int multiple = 0;
         while(multiple <= gcd) {
