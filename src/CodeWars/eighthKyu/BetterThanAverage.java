86553404dba3b6b1acbcceb0d6aa25fcff773fa8
JayanDesilva <jaycdesilva@gmail.com>
2023-11-13 07:44:04 -0500

    betterThanAverage

---
+++ src/CodeWars/eighthKyu/BetterThanAverage.java
@@ -0,0 +1,22 @@
+package CodeWars.eighthKyu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BetterThanAverage {
+    public static void main(String[] args) {
+
+        int[] t = {5,5,5,5,5};
+        System.out.println(betterThanAverage(t,5));
+    }
+    public static boolean betterThanAverage (int[] classPoints, int yourPoints) {
+        double sum = Arrays.stream(classPoints).sum() + yourPoints;
+        double avg = sum/(double) (classPoints.length + 1);
+        return  avg < yourPoints;
+
+//        return (Arrays.stream(classPoints).sum() + yourPoints)/classPoints.length + 1 <= yourPoints;
+
+    }
+}
