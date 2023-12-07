e463ef2f28085a341dfd8e19fbd2fe899af991df
JayanDesilva <jaycdesilva@gmail.com>
2023-12-07 04:56:52 -0500

    day 2 completed with counting sort

--- src/Hackerrank/oneweekpreparation/daytwo/CountingSort.java
+++ src/Hackerrank/oneweekpreparation/daytwo/CountingSort.java
@@ -1,16 +1,41 @@
 package Hackerrank.oneweekpreparation.daytwo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CountingSort {
     public static void main(String[] args) {
 
-        String str = "Kayyappa";
-        String newString = str.substring(1,str.length()-1);
-        System.out.println(newString);
+        int[] arr1 = {  63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9 ,53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89,
+                        30, 27, 79,46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9,
+                        69, 48, 33,60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33};
+
+        List<Integer> list = Arrays.stream(arr1).boxed().collect(Collectors.toList());
+
+        int sum = 0;
+        for(int i: arr1)  {
+            if(i == 1) sum++;
+        }
+        System.out.println("sum of 63 : " +sum);
+        countingSort(list);
     }
 
     public static List<Integer> countingSort (List<Integer> arr) {
+
+        int min = 0;
+        int max = 100;
+        int range = max - min ;
+
+        //Array to count each element instance.
+        int[] elementCountArr = new int[range];
+
+        //Store occurrences of each element
+        for(int i: arr) {
+            elementCountArr[i]++;
+        }
+        System.out.println(Arrays.toString(elementCountArr));
         return null;
     }
 }
